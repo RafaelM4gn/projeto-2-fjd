f2e1f336d90a1f5b54b391fe2dd2f0cbc56c839a
RafaelM4gn <rfl.magno02@gmail.com>
2023-06-24 18:51:17 -0300

    feature: initial commit

---
+++ scripts/main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+# Botão Play
+onready var button_play = $ButtonPlay
+
+# Botão Quit
+onready var button_quit = $ButtonQuit
+
+func _ready():
+	# Conecte os sinais dos botões às funções correspondentes
+	button_play.connect("pressed", self, "_on_PlayButton_pressed")
+	button_quit.connect("pressed", self, "_on_QuitButton_pressed")
+
+func _on_PlayButton_pressed():
+	# Carrega e muda para a cena level0.tscn quando o botão Play é pressionado
+	var level = load("res://scenes/levels/level0.tscn")
+	get_tree().change_scene_to(level)
+
+func _on_QuitButton_pressed():
+	# Fecha o jogo quando o botão Quit é pressionado
+	get_tree().quit()
---
+++ scripts/player.gd
@@ -0,0 +1,36 @@
+extends KinematicBody2D
+
+export var speed = 200
+export var dash_speed = 600
+export var dash_duration = 0.3
+var is_dashing = false
+var dash_direction = Vector2()
+
+func _physics_process(delta):
+	var direction = Vector2(0, 0)
+
+	if is_dashing:
+		move_and_slide(dash_direction * dash_speed)
+	else:
+		if Input.is_action_pressed("move_right"):
+			direction.x += 1
+		if Input.is_action_pressed("move_left"):
+			direction.x -= 1
+		if Input.is_action_pressed("move_down"):
+			direction.y += 1
+		if Input.is_action_pressed("move_up"):
+			direction.y -= 1
+		direction = direction.normalized()
+		move_and_slide(direction * speed)
+	
+		if Input.is_action_just_pressed("dash"):
+			start_dash(direction)
+
+func start_dash(direction):
+	if !is_dashing and direction.length() > 0:
+		is_dashing = true
+		dash_direction = direction.normalized()
+		get_tree().create_timer(dash_duration).connect("timeout", self, "end_dash")
+
+func end_dash():
+	is_dashing = false
